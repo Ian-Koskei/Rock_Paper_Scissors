c9349454c00ae941b04a6829258a0875a41988c6
Ian-Koskei <ian.k.kipkoech@gmail.com>
2025-07-21 15:17:03 +0300

    Added rounds missing score increment

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -20,22 +20,40 @@ let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice) {
-for (let round = 0; round < 5; round++){
+
     if (humanChoice === 'rock' && computerChoice === 'paper'){
-        console.log('You lose! Rock beats paper')
+        console.log('You lose! paper beats rock')
         computerScore = computerScore++
     }
     else if(humanChoice === "scissors" && computerChoice === "paper"){
         console.log("You win! scissors beats paper")
         humanScore = humanScore++
     }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! paper beats rock")
+        humanScore = humanScore++
+    }
+    else if(humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! rock beats scissors")
+        computerScore = computerScore++
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! scissors beats paper")
+        computerScore = computerScore++
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You lose! rock beats scissors")
+        computerScore = computerScore++
+    }
     else{
         console.log("It's a tie")
     }
 }
-}
+
 
 const humanSelection = getHumanChoice()
 const computerSelection = getComputerChoice()
 
 playRound(humanSelection, computerSelection)
+}
+console.log(humanScore)
